7fef25747c48610b881a08f20916f881201befc4
Andreh <czuber@powernote.localdomain>
2015-10-14 00:50:43 -0300

    Mudanca na estrategia do entityManager

---
+++ src/main/java/com/pizzaria/listener/EntityManagerLoaderListener.java
@@ -0,0 +1,97 @@
+package com.pizzaria.listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.pizzaria.util.HerokuURLAnalyser;
+
+@WebListener
+public class EntityManagerLoaderListener implements ServletContextListener{
+	private static final Logger logger = LoggerFactory.getLogger(EntityManagerLoaderListener.class);
+
+    private String DEFAULT_DB_URL = "jdbc:hsqldb:mem:testdb";
+    private static EntityManagerFactory emf;
+    private boolean pushAdditionalProperties = true;
+
+    public EntityManagerLoaderListener() {
+    }
+
+    public EntityManagerLoaderListener(boolean pushAdditionalProperties) {
+        this.pushAdditionalProperties = pushAdditionalProperties;
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        logger.debug("WebListener start entity manager");
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        if (databaseUrl == null && pushAdditionalProperties) {
+            logger.debug("Use default config in persistence.xml with " + DEFAULT_DB_URL);
+            databaseUrl = DEFAULT_DB_URL;
+        }
+        Map<String, String> properties = new HashMap<String, String>();
+        if (pushAdditionalProperties) {
+            HerokuURLAnalyser analyser = new HerokuURLAnalyser(databaseUrl);
+
+            logger.debug("SET JDBC URL TO " + analyser.getJdbcURL());
+            properties.put("javax.persistence.jdbc.url", analyser.getJdbcURL());
+            properties.put("javax.persistence.jdbc.user", analyser.getUserName());
+            properties.put("javax.persistence.jdbc.password", analyser.getPassword());
+
+            if ("postgres".equals(analyser.getDbVendor())) {
+                logger.debug("SET DRIVER FOR postgres");
+                properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+                properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            } else if ("h2".equals(analyser.getDbVendor())) {
+                logger.debug("SET DRIVER FOR h2");
+                properties.put("javax.persistence.jdbc.driver", "org.h2.Driver");
+            }
+        }
+        emf = Persistence.createEntityManagerFactory("pizzaria", properties);
+
+
+    }
+
+    /**
+     * Close the entity manager
+     *
+     * @param event ServletContextEvent not used
+     */
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        emf.close();
+    }
+
+    /**
+     * Create the EntityManager
+     *
+     * @return EntityManager
+     */
+    public static EntityManager createEntityManager() {
+        if (emf == null) {
+            throw new IllegalStateException("Context is not initialized yet.");
+        }
+
+        return emf.createEntityManager();
+    }
+
+
+    public boolean isPushAdditionalProperties() {
+        return pushAdditionalProperties;
+    }
+
+    public void setPushAdditionalProperties(boolean pushAdditionalProperties) {
+        this.pushAdditionalProperties = pushAdditionalProperties;
+    }
+
+}
